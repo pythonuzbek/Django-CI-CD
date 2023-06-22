abdc25f86bc65ee92dfe357a6155c9e2404d8ab5
pythonuzbek <nusratullo3304@gmail.com>
2023-06-22 18:32:34 +0500

    change settings.py

--- root/settings.py
+++ root/settings.py
@@ -55,8 +55,12 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
 
