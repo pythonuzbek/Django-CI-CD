0aa7354d3db489cd5339d6940f703e30c71dae9c
pythonuzbek <nusratullo3304@gmail.com>
2023-06-22 20:11:21 +0500

    change urls.py

--- root/urls.py
+++ root/urls.py
@@ -8,7 +8,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Django CI/CD API",
+      title="Django CI/CD API Qalay",
       default_version='v1',
       description="CI/CD Django",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -23,4 +23,6 @@ urlpatterns = [
     path('',include('apps.urls')),
     path('admin/', admin.site.urls)
 ]
+
+error
 
