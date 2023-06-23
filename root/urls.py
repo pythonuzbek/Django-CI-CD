9a275cbb9f2d5dcf05d73aadfeb0daebb859b9c4
pythonuzbek <nusratullo3304@gmail.com>
2023-06-23 09:33:49 +0500

    change urls.py

--- root/urls.py
+++ root/urls.py
@@ -8,7 +8,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Django CI/CD API FOR TEST",
+      title="Django CI/CD API FOR TEST1",
       default_version='v1',
       description="CI/CD Django",
       terms_of_service="https://www.google.com/policies/terms/",
