1362eaad05b9c6c33a4cd0badef6df158c95fe4c
pythonuzbek <nusratullo3304@gmail.com>
2023-06-23 09:40:19 +0500

    change urls.py

--- root/urls.py
+++ root/urls.py
@@ -4,25 +4,20 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Django CI/CD API FOR TEST1",
-      default_version='v1',
-      description="CI/CD Django",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Django CI/CD API Nusratullo",
+        default_version='v1',
+        description="CI/CD Django",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('',include('apps.urls')),
+    path('', include('apps.urls')),
     path('admin/', admin.site.urls)
 ]
-
-
-
